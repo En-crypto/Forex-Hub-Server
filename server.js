eb2c2f82658acae3bf832813c653069db8ecc454
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-06-04 19:40:14 +0300

    server init

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict'
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { default: axios } = require('axios');
+const server = express();
+server.use(cors());
+server.get('/', (req, res) => { 
+       res.status(200).send('Home');
+    })
+server.listen(3001, () => { 
+       console.log(`Listening on PORT http://localhost:3001/`);
+    })
